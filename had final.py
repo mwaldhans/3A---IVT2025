345de009a95a1f60059540f663ca09a161b7aeba
Marek Waldhans <marek.waldhans@gymkren.cz>
2026-02-02 11:16:53 +0100

    had

---
+++ had final.py
@@ -0,0 +1,92 @@
+import pygame
+
+# Inicializace Pygame
+pygame.init()
+pygame.font.init()
+
+# Inicializace hodin pro rizeni snimkovani
+hodiny = pygame.time.Clock()
+
+#nastaveni rychlosti
+fps=5
+
+#nastaveni pocatecni delky hada
+delka=15
+
+# Nastaveni rozmeru okna
+sirka_okna = 800
+vyska_okna = 600
+okno = pygame.display.set_mode((sirka_okna, vyska_okna))
+pygame.display.set_caption("Had")
+
+# Barvy
+barva_hlava = (255,0, 0)  # červena barva
+barva_hada = (0, 255, 0)  # zelena barva
+barva_pozadi = (0,0, 0)    # cerna barva
+
+# Velikost ctverce
+velikost_hada = 10
+
+# Pocatecni pozice hada
+pozice= [[sirka_okna // 2,vyska_okna // 2]]
+
+# Pocatecni smer
+smer=[1,0]
+
+
+        
+
+# Hlavni smycka
+program_bezi = True
+while program_bezi:
+    for udalost in pygame.event.get():
+        if udalost.type == pygame.QUIT:
+            program_bezi = False
+
+    # Ziskani aktualniho stavu klaves
+    stav_klaves = pygame.key.get_pressed()
+
+    # Pohyb podle stisknutych klaves
+    if stav_klaves[pygame.K_UP]:  # Sipka nahoru
+        smer=[0.2,-1]
+    if stav_klaves[pygame.K_DOWN]:  # Sipka dolu
+        smer=[1,1]
+    if stav_klaves[pygame.K_LEFT]:  # Sipka vlevo
+        smer=[-1,1]
+    if stav_klaves[pygame.K_RIGHT]:  # Sipka vpravo
+        smer=[1,1]
+
+
+    
+    # Vyplneni pozadi
+    okno.fill(barva_pozadi)
+
+    # Vykresleni hada
+    
+     
+    
+    
+    for had in pozice:
+        pygame.draw.rect(okno, barva_hada, (had[0], had[1], velikost_hada, velikost_hada))
+
+    #posuneme hada
+    x=pozice[0][0]+velikost_hada*smer[0]
+    y=pozice[0][1]+velikost_hada*smer[1]
+  
+    pozice.insert(0,[x,y])
+    pozice=pozice[:delka]
+    
+    
+    my_font = pygame.font.SysFont('arial', 30)
+    text_surface = my_font.render('skore: '+str(x), True, (255, 0, 0))
+    okno.blit(text_surface, (10, 10))
+    
+    
+    # Aktualizace obrazovky
+    pygame.display.flip()
+  
+    # Nastaveni FPS na 60 snimku za sekundu
+    hodiny.tick(fps)
+
+# Ukonceni Pygame
+pygame.quit()
